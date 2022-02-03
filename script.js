b58e97931bd758bcfa81f3427931feea698e755a
babymonie <62741287+babymonie@users.noreply.github.com>
2022-02-03 20:18:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     document.getElementById("url").focus();
+    alert("sorry about this but the youtu.be domain is not supported yet.\nPlease enter a the normal youtube.com URL");
 }
 function check() {
     var url = document.getElementById("url").value;
@@ -10,30 +11,53 @@ function check() {
     else
     {
         //check if the url contains youtube.com or youtu.be
-        if (url.indexOf("youtube.com") == -1 && url.indexOf("youtu.be") == -1) {
-            alert("Please enter a valid YouTube URL");
+        if (url.indexOf("youtube.com") == -1) {
+            alert("Please enter a valid youtube URL");
             return false;
         }
         else {
-                //get anything afte v=
-                var video_id = url.substring(url.indexOf("v=") + 2);
+                //get anything afteR v=
+                var video_id = url.substring(url.indexOf("v=") + 2||url.indexOf("/") + 1);
                 //check if the video id is valid
                 if (video_id.length == 11) {
-                    var url2 = "https://i.ytimg.com/vi/"+video_id+"/hqdefault.jpg";
-                    //download the image                    
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', url2, true);
-                    xhr.responseType = 'blob';
-                    xhr.onload = function(e) {
-                        if (this.status == 200) {
-                            var blob = this.response;
-                            var link = document.createElement('a');
-                            link.href = window.URL.createObjectURL(blob);
-                            link.download = video_id+".jpg";
-                            link.click();
+                   fetch("https://noembed.com/embed?url=" + url)
+                    .then(response => {
+                        text = response.text();
+                        console.log(text);
+                        return text;
+                    })
+                    .then(data => {
+                        if (data.error) {
+                            alert("Please enter a valid YouTube URL");
                         }
-                    };
-                    xhr.send();
+                        else {
+                            console.log(data);
+                            data = JSON.parse(data);
+                            var title = data.title;
+                            console.log(title);
+                            var thumbnail_url = data.thumbnail_url;
+                            console.log(thumbnail_url);
+                            fetch(thumbnail_url)
+                            .then(response => {
+                                return response.blob();
+                            })
+                            .then(blob => {
+                                console.log(blob);
+                                var url = URL.createObjectURL(blob);
+                                console.log(url);
+                                var link = document.createElement("a");
+                                link.href = url;
+                                link.download = title + ".jpg";
+                                link.click();
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
                 }
                 else {
                     alert("Please enter a valid YouTube URL");
